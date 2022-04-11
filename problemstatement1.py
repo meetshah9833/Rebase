8eddca06499c1c53396ecf27e68dd24a0924be6d
meetshah9833 <meetnshah9833@gmail.com>
2022-04-11 16:59:15 +0530

    Add files via upload

---
+++ problemstatement1.py
@@ -0,0 +1,9 @@
+ def ratioFunction():
+        num1 = input('Enter the first number: ')
+        num1 = int(num1) # Now we are good
+        num2 = input('Enter the second number: ')
+        num2 = int(num2) # Good, good
+        ratio12 = int(num1/num2)
+     print('The ratio of', num1, 'and', num2,'is', str(ratio12) + '.')
+if ratio12 > 1.8 && < 1.9:
+    print("Eureka" + num1 "," + num2)
